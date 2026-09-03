cbf256afa40dbae8474c64060a88b248113a9b54
krishcrypt <krishm1510@gmail.com>
2026-09-04 00:53:28 +0530

    Add responsive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <Home />
+  return <Navbar />;
 }
 
-export default App
+export default App;
