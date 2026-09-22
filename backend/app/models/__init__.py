a999376b73d590e27222bb2e0372a4cd7a3ea854
Nik-ERROR-exe <wnikhil146@gmail.com>
2026-09-22 06:41:35 +0530

    Update models init

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,16 @@
 from app.models.user import User, AdminProfile
 from app.models.event import Event
 from app.models.registration import EventRegistration, RegistrationMember, AuditLog
+from app.models.admin_action_log import AdminActionLog
+
+try:
+    from app.models.early_bird import EarlyBirdPricing
+except Exception:
+    EarlyBirdPricing = None  # not present; autogenerate won't miss if added later
+try:
+    from app.models.review import RegistrationReview
+except Exception:
+    RegistrationReview = None
 
 __all__ = [
     "User",
@@ -9,4 +19,7 @@ __all__ = [
     "EventRegistration",
     "RegistrationMember",
     "AuditLog",
+    "AdminActionLog",
+    "EarlyBirdPricing",
+    "RegistrationReview",
 ]
